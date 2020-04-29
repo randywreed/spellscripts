eb943e2d29332fe805e9c83a2fd72655f1485bb9
Randy Reed <randywreed@gmail.com>
2020-04-28 23:27:53 -0400

    first commit

---
+++ spell_run.py
@@ -0,0 +1,13 @@
+import spell.client
+client=spell.client.from_environment()
+prompt='the crux of calvinism is the soverignty of God. If god is soverign, then god controls everything. Thus the doctrin of predestination holds'
+cmd="python gen_gpt2.py --prompt '"+prompt+"'"
+run=client.runs.new(command=cmd,machine_type='k80x2-prempt',\
+    github_url="https://github.com/randywreed/gpt2-generate.git", \
+    github_ref="master",pip_packages=["gpt-2-simple"], \
+    attached_resources={'runs/54/models':'/spell/models'})
+run.wait_status(*client.runs.FINAL)
+print(run.id)
+run.cp("simple_results.csv")
+for line in run.logs():
+    print(line)
